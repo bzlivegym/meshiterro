8a45183352ce76fe505b58100c8ce8ff3af63914
bzlivegym <gibsonraybantak@gmail.com>
2021-12-16 14:24:20 +0000

    [update]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '18aa95b2d98235e31cac028360177d4c395b903ef08918f9f5bf8cc04d8e47a24ae4fd1dae9c5744006289af9156c4cd26fb6ac23fa46b098cc3e7cc9ed206c0'
